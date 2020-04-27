def2bc8bd65c8f766353475568495299ff23a208
ParthhSharma <53979691+ParthhSharma@users.noreply.github.com>
2020-04-27 18:50:03 +0530

    Add files via upload

---
+++ score.py
@@ -0,0 +1,45 @@
+score = input("Enter Score: ")
+
+s=float(score)
+
+if s<0.0 :
+
+    print("error")
+
+elif s>1.0 :
+
+    print("error")
+
+elif s>=0.9 :
+
+	grade='A'
+
+    print(grade)
+
+elif s>=0.8 :
+
+	grade='B'
+
+    print(grade)
+
+elif s>=0.7 :
+
+	grade='C'
+
+    print(grade)
+
+elif s>=0.6 :
+
+	grade='D'
+
+    print(grade)
+
+elif s<0.6 :
+
+	grade='F'
+
+    print(grade)
+
+else :
+
+    print("you had entered a wrong input")

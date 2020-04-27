65c277a9c8872c45b3069435e0a2d4089d018440
ParthhSharma <53979691+ParthhSharma@users.noreply.github.com>
2020-04-27 18:50:18 +0530

    Add files via upload

---
+++ simpint.py
@@ -0,0 +1,9 @@
+#TO CALCULATE SIMPLE INTEREST
+p=input("Enter the principal amount (in INR) = ")
+p=float(p)
+r=input("Enter the rate of interest (%) =  ")
+r=float(r)
+t=input("Enter the time span (in years) = ")
+t=float(t)
+simple_interest=p*r*t/100
+print("total interest calculated to be INR ",simple_interest)

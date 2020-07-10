e4f0921dd4cd9f8b97997260a45b16250ec9352f
Parth Sharma <53979691+ParthhSharma@users.noreply.github.com>
2020-07-10 08:58:22 +0530

    Add files via upload

---
+++ CompareTheTriplets.py
@@ -0,0 +1,95 @@
+# Comparison points is the total points a person earned.
+#
+# Given a and b, determine their respective comparison points.
+#
+# Example
+#
+# a = [1, 2, 3]
+# b = [3, 2, 1]
+# For elements *0*, Bob is awarded a point because a[0] .
+# For the equal elements a[1] and b[1], no points are earned.
+# Finally, for elements 2, a[2] > b[2] so Alice receives a point.
+# The return array is [1, 1] with Alice's score first and Bob's second.
+#
+# Function Description
+#
+# Complete the function compareTriplets in the editor below.
+#
+# compareTriplets has the following parameter(s):
+#
+# int a[3]: Alice's challenge rating
+# int b[3]: Bob's challenge rating
+# Return
+#
+# int[2]: Alice's score is in the first position, and Bob's score is in the second.
+# Input Format
+#
+# The first line contains 3 space-separated integers, a[0], a[1], and a[2], the respective values in triplet a.
+# The second line contains 3 space-separated integers, b[0], b[1], and b[2], the respective values in triplet b.
+#
+# Constraints
+#
+# 1 ≤ a[i] ≤ 100
+# 1 ≤ b[i] ≤ 100
+# Sample Input 0
+#
+# 5 6 7
+# 3 6 10
+# Sample Output 0
+#
+# 1 1
+# Explanation 0
+#
+# In this example:
+#
+# Now, let's compare each individual score:
+#
+# , so Alice receives  point.
+# , so nobody receives a point.
+# , so Bob receives  point.
+# Alice's comparison score is , and Bob's comparison score is . Thus, we return the array .
+#
+# Sample Input 1
+#
+# 17 28 30
+# 99 16 8
+# Sample Output 1
+#
+# 2 1
+# Explanation 1
+#
+# Comparing the  elements,  so Bob receives a point.
+# Comparing the  and  elements,  and  so Alice receives two points.
+# The return array is .
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    for i in range(len(a)):
+        if a[i]>b[i]:
+            alice += 1
+        elif a[i]<b[i]:
+            bob += 1
+    return [alice,bob]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
